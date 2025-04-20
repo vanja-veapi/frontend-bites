5746c2934d834b697d7c26745ec5150f0ab8d719
vanja-veapi <vanja.veapi@gmail.com>
2025-04-20 22:35:09 +0200

    add error handling for logo and boots elements, and refactor boot animation logic

--- slider-css/main.js
+++ slider-css/main.js
@@ -19,7 +19,10 @@ const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const changeLogo = async () => {
 	const logo = document.querySelector('.logo');
+	if (!logo) throw new Error('Logo not found');
+
 	const logoImg = logo.getElementsByTagName('img')[0];
+	if (!logoImg) return;
 
 	logo.style.transform = 'translateY(-164px)';
 
@@ -30,23 +33,31 @@ const changeLogo = async () => {
 	logo.style.transform = '';
 };
 
+const BOOTS_START_DISTANCE = '-800px';
+const BOOTS_END_DISTANCE = '800px';
+
 const changeBoots = async () => {
 	const bootsImg = document.querySelector('.boots img');
+	if (!bootsImg) throw new Error('Boots not found');
 
-	bootsImg.style.transform = 'translateX(800px)';
+	bootsImg.style.transform = `translateX(${BOOTS_END_DISTANCE})`;
 
+	// * Hiding boots, because on line 45, I'm moving new boots behind left edge of div, to get smooth slider effect
 	bootsImg.classList.add('hidden');
 	await sleep(500);
-	bootsImg.style.transform = 'translateX(-800px)';
+	bootsImg.style.transform = `translateX(${BOOTS_START_DISTANCE})`;
 	await sleep(500);
 	bootsImg.classList.remove('hidden');
 	bootsImg.style.transform = '';
 };
 
+const changeText = () => {};
+
 const nextSlide = () => {
 	console.log('Next slide...');
 	changeLogo();
 	changeBoots();
+	changeText();
 };
 
 const previousSlide = () => console.log('Previous slide...');
