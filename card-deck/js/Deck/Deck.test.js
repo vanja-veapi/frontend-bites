653185eee139e21d310241ccb02f6f7394fcd857
vanja-veapi <vanja.veapi@gmail.com>
2025-12-07 20:29:43 +0100

    Refactor Deck class and add comprehensive tests; implement Sharingan Eyes HTML and CSS

---
+++ card-deck/js/Deck/Deck.test.js
@@ -0,0 +1,17 @@
+describe('Testing Deck class', () => {
+	test('Shuffle an empty deck', () => {
+		throw new Error('Greska');
+	});
+
+	test('Draw <n> card(s) from the top', () => {
+		throw new Error('Greska');
+	});
+
+	test('Draw <n> card(s) from the bottom', () => {
+		throw new Error('Greska');
+	});
+
+	test('Try to draw card from empty deck', () => {
+		throw new Error('Greska');
+	});
+});
