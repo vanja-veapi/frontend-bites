d36b5d36eedc521e8791794941fdca88a4a70143
vanja-veapi <vanja.veapi@gmail.com>
2026-02-04 22:18:19 +0100

    feat: Add optimized solution for removing duplicates from an array

--- alghoritms/withoutDuplicates.ts
+++ alghoritms/withoutDuplicates.ts
@@ -4,5 +4,17 @@
 // Example [1,2,3,5,2,3,1,6,7]
 // New array [5,6,7]
 
+// Solution 1: O(n^2)
 const withoutDuplicates = (array: number[]) =>
 	array.filter((item) => array.indexOf(item) === array.lastIndexOf(item));
+
+// Solution 2: O(2n) JER IMAM DVE PROLAZA KROZ NIZ
+const withoutDuplicates_Big_o = (array: number[]) => {
+	const count = new Map<number, number>();
+
+	for (const num of array) {
+		count.set(num, (count.get(num) || 0) + 1);
+	}
+
+	return array.filter((num) => count.get(num) === 1);
+};
