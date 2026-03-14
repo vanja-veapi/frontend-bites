20023267039d1e3e3e31c5a9b887cdc49b0bf8e0
vanja-veapi <vanja.veapi@gmail.com>
2026-03-14 18:23:40 +0100

    #

--- alghoritms/big-o/binary-search-log-n.js
+++ alghoritms/big-o/binary-search-log-n.js
@@ -1,23 +1,45 @@
-function nadjiPetrovic(imenik) {
-	let levo = 0;
-	let desno = imenik.length - 1;
+function binarySearch(arr, target) {
+	let leftIndex = 0;
+	let rightIndex = arr.length - 1;
 
-	while (levo <= desno) {
-		const sredina = Math.floor((levo + desno) / 2);
-		const prezime = imenik[sredina];
+	if (!arr || arr.length === 0) throw new Error('Niz ne sme biti prazan');
+	if (target == undefined) throw new Error('Nije unet trazeni broj');
 
-		const poređenje = prezime.localeCompare('Petrović', 'sr');
+	while (leftIndex <= rightIndex) {
+		const midIndex = Math.floor((leftIndex + rightIndex) / 2);
 
-		if (poređenje === 0) {
-			return 'Nađen Petrović';
-		}
+		if (arr[midIndex] === target) return arr[midIndex];
 
-		if (poređenje < 0) {
-			levo = sredina + 1;
+		if (arr[midIndex] < target) {
+			leftIndex = midIndex + 1; // Suzavamo pretragu na desnu stranu niza za jedan jer znamo da nije taj element zbog prethodne provere
 		} else {
-			desno = sredina - 1;
+			rightIndex = midIndex - 1; // Suzavamo pretragu na levu stranu niza za jedan jer znamo da nije taj element zbog prethodne provere
 		}
 	}
 
-	return 'Petrović nije u imeniku';
+	return 'Element nije pronađen u nizu';
 }
+
+// function nadjiPetrovic(imenik) {
+// 	let levo = 0;
+// 	let desno = imenik.length - 1;
+
+// 	while (levo <= desno) {
+// 		const sredina = Math.floor((levo + desno) / 2);
+// 		const prezime = imenik[sredina];
+
+// 		const poređenje = prezime.localeCompare('Petrović', 'sr');
+
+// 		if (poređenje === 0) {
+// 			return 'Nađen Petrović';
+// 		}
+
+// 		if (poređenje < 0) {
+// 			levo = sredina + 1;
+// 		} else {
+// 			desno = sredina - 1;
+// 		}
+// 	}
+
+// 	return 'Petrović nije u imeniku';
+// }
